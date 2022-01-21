bec4661b6e3ad717467adbc1bde81b86bf3a427e
harry2394 <harr.render@northlink.digital>
2022-01-21 16:09:54 +0000

    new blog layout and search components

--- astro/tailwind.config.js
+++ astro/tailwind.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
+  // plugins: [
+  //   // ...
+  //   require('@tailwindcss/forms'),
+  // ],
 };
